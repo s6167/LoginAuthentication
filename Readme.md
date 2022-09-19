87efa83c19ce0293789a336b7130ef8ce5418b95
Sanjay <sanjayjadhav465@gmail.com>
2022-09-19 17:40:33 +0530

    First Commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+<h2>Introduction</h2>
+<p>This is JavaScript Mini project. This project demostrate the Login Authentication</p>
+
+<h2>Authenticatons</h2>
+<h3>Username Authentication:</h3>
+<p>In this project, email id of user is expected as username. So, email Id need to have Speciafic structure to be valid.</p>
+<p>All required validation of Email id are done using JavaScrip</p>
+
+<h3>Password Authentication: <h3>
+<p>In this project, password entered by user is also validiated </p>
+<p>password contains combination of characters. There are four types of Validations Implimated for Password</p>
+<ul>
+<li>Password Should Contains LowerCase Character</li>
+<li>Password Should contain upper case character</li>
+<li>password should contain at least 1 number </li>
+<li>Password should have min length of 8 characters</li>
+</ul>
+
+<h2>References<h2>
+<p>Some online resources are refered to built this project </p>
+
+
+<h2>Thank you</h2>
---
+++ index.js
@@ -0,0 +1,73 @@
+
+function checkUserName(){
+    console.log("Checking UserName")
+    let username= document.getElementById("username").value
+
+    let check = true;
+    if(check){
+
+        document.getElementById("error-username").innerHTML=""
+    }
+
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(username)){
+        check=true;
+    }
+    else{
+        document.getElementById("error-username").innerHTML="Enter Valid Email"
+    }
+}
+
+var myInput = document.getElementById("psw");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+
+myInput.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+}
+
+myInput.onblur = function() {
+    document.getElementById("message").style.display = "none";
+}
+
+myInput.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if(myInput.value.match(lowerCaseLetters)) {  
+      letter.classList.remove("invalid");
+      letter.classList.add("valid");
+    } else {
+      letter.classList.remove("valid");
+      letter.classList.add("invalid");
+    }
+    
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if(myInput.value.match(upperCaseLetters)) {  
+      capital.classList.remove("invalid");
+      capital.classList.add("valid");
+    } else {
+      capital.classList.remove("valid");
+      capital.classList.add("invalid");
+    }
+  
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if(myInput.value.match(numbers)) {  
+      number.classList.remove("invalid");
+      number.classList.add("valid");
+    } else {
+      number.classList.remove("valid");
+      number.classList.add("invalid");
+    }
+    
+    // Validate length
+    if(myInput.value.length >= 8) {
+      length.classList.remove("invalid");
+      length.classList.add("valid");
+    } else {
+      length.classList.remove("valid");
+      length.classList.add("invalid");
+    }
+}
